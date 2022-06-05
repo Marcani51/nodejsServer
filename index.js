92c7df5fc54feac3a12b1139ee1861375c5d34e8
Marcellus Denta Widyapramana <marcellus.widyapramana@multipolar.com>
2022-06-06 01:28:06 +0700

    req and res

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const port="3000"
+const app = express()
+console.dir(app)
+app.use((req,res)=>{
+    console.log("new request")
+    res.send("<input type='text' />");
+})
+app.listen(port,()=>{
+    console.log("listen on port 3000");
+})
